1a17294a92806fc433a76fb0ad65951ade4afb57
Ankit Seth <iam4nkit@gmail.com>
2022-10-21 00:13:44 +0530

    added backend connectivity

--- src/components/LoginPage/SignUp.js
+++ src/components/LoginPage/SignUp.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SignUp.css'
 import image from '../../images/img3.png'
 import { Link } from 'react-router-dom'
 import TextField from '@material-ui/core/TextField';
-import { Button } from '@mui/material';
+import { Alert, Button } from '@mui/material';
+import UserService from '../../services/UserService';
 
 function SignUp() {
+
+    const [user, setUser] = useState({
+      name: "", 
+      phone: "",
+      email: "",
+      password: "",
+    });
+  
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setUser({...user,[e.target.name] : value});
+    console.log(user);
+  }
+
+  const saveUser = (e) => {
+    e.preventDefault();
+    UserService.saveUser(user)
+      .then((response) => {
+        console.log(response);
+        alert("User is successfully registered");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className='signup-body-container'>
         <div className='signup-form-container'>
@@ -14,14 +41,14 @@ function SignUp() {
             <h3>Welcome to our Page <Link className='login-link' to = "/login">SignIn</Link></h3>
           </div>
           <div className='signup-form'>
-            <TextField id="standard-basic" type="name" label="Full Name" variant="outlined" />
-            <TextField id="standard-basic" type="phonenumber" label="Phone Number" variant="outlined" />
-            <TextField id="standard-basic" type="email" label="Email" variant="outlined" />
-            <TextField className='login-text' id="standard-basic" type="password" label="Password" variant="outlined" />
+            <TextField name='name' value={user.name} onChange={(e) => handleChange(e)} id="standard-basic" type="name" label="Full Name" variant="outlined" />
+            <TextField name='phone' value={user.phone} onChange={(e) => handleChange(e)} id="standard-basic" type="phonenumber" label="Phone Number" variant="outlined" />
+            <TextField name='email' value={user.email} onChange={(e) => handleChange(e)} id="standard-basic" type="email" label="Email" variant="outlined" />
+            <TextField name='password' value={user.password} onChange={(e) => handleChange(e)} id="standard-basic" type="password" label="Password" variant="outlined" />
           </div>
           <div className='signup-buttons'>
-              <Button variant='contained'>Sign In</Button>
-              <Button variant='contained'>Sign In as Seller</Button>
+              <Button onClick={saveUser} variant='contained'>Sign Up</Button>
+              <Button variant='contained'>Sign Up as Seller</Button>
           </div>
         </div>
         <div className='signup-image-container'>
