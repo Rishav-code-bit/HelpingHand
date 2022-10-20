5e2979f4b993bcc094726480089309e4457ad0bc
Ankit Seth <iam4nkit@gmail.com>
2022-10-20 13:35:48 +0530

    design changes

--- src/components/Homepage/Home.js
+++ src/components/Homepage/Home.js
@@ -7,15 +7,17 @@ function Home() {
     <div className='home-container'>
         <div className='home-navbar'>
         <div className="home-navbar-container">
-          <div classname='home-title'>SDP Project</div>
+          <div classname='home-title'>
+            <h1>SDP App</h1>
+          </div>
             <div className='home-lists'>
                 <ul>
                     <li>Home</li>
                     <li>About</li>
                     <li>Github</li>
                     <li>Contact</li>
-                    <Button className='login-button' variant="contained">Login</Button>
-                    <Button className='signup-button' variant="contained">Sign Up</Button>
+                    <li><Button className='login-button' variant="contained">Login</Button></li>
+                    <li><Button className='signup-button' variant="contained">Sign Up</Button></li>
                 </ul>
             </div>
         </div>
