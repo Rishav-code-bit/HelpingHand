93c6bf460d3684f5fd3178a3ef2c141ffa78eb43
Ankit Seth <iam4nkit@gmail.com>
2022-10-25 18:21:45 +0530

    made design

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -2,21 +2,9 @@ import React from 'react'
 import './Login.css'
 import image from '../../images/img2.png'
 import { Link } from 'react-router-dom'
-import TextField from '@material-ui/core/TextField';
 import { Button } from '@mui/material';
-import { withStyles } from '@material-ui/core/styles';
 
 function Login() {
-
-  const CssTextField = withStyles({
-     root: { 
-      '& label.Mui-focused': { color: 'white', },
-      '& .MuiInput-underline:after': { borderBottomColor: 'yellow', }, 
-      '& .MuiOutlinedInput-root': { '& fieldset': { borderColor: 'white', }, 
-      '&:hover fieldset': { borderColor: 'white', }, 
-      '&.Mui-focused fieldset': { borderColor: 'yellow', }, }, }, 
-  })(TextField);
-
 
   return (
     <div className='login-body-container'>
@@ -26,8 +14,8 @@ function Login() {
             <h3>Welcome to our Page <Link className='login-link' to = "/signup">SignUp</Link></h3>
           </div>
           <div className='login-form'>
-            <CssTextField id="standard-basic" type="email" label="Email" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}}/>
-            <CssTextField className='login-text' id="standard-basic" type="password" label="Password" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}} />
+            <input className='login-input' placeholder='Email Address' id="standard-basic" type="email" label="Email" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}}/>
+            <input className='login-input' placeholder='Password' id="standard-basic" type="password" label="Password" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}} />
           </div>
           <div className='login-buttons'>
               <Button className='seller-button-user' variant='contained'>Sign In</Button>
--- src/components/LoginPage/SignUp.js
+++ src/components/LoginPage/SignUp.js
@@ -2,21 +2,10 @@ import React, { useState } from 'react'
 import './SignUp.css'
 import image from '../../images/img3.png'
 import { Link } from 'react-router-dom'
-import TextField from '@material-ui/core/TextField';
 import { Button } from '@mui/material';
 import UserService from '../../services/UserService';
-import { withStyles } from '@material-ui/core/styles';
 
 function SignUp() {
-
-  const CssTextField = withStyles({
-    root: { 
-     '& label.Mui-focused': { color: 'white', },
-     '& .MuiInput-underline:after': { borderBottomColor: 'yellow', }, 
-     '& .MuiOutlinedInput-root': { '& fieldset': { borderColor: 'white', }, 
-     '&:hover fieldset': { borderColor: 'white', }, 
-     '&.Mui-focused fieldset': { borderColor: 'yellow', }, }, }, 
-  })(TextField);
 
     const [user, setUser] = useState({
       name: "", 
@@ -51,10 +40,10 @@ function SignUp() {
             <h3>Welcome to our Page <Link className='login-link' to = "/login">SignIn</Link></h3>
           </div>
           <div className='signup-form'>
-            <CssTextField name='name' value={user.name} onChange={(e) => handleChange(e)} id="standard-basic" type="name" label="Full Name" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}} />
-            <CssTextField name='phone' value={user.phone} onChange={(e) => handleChange(e)} id="standard-basic" type="phonenumber" label="Phone Number" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}} />
-            <CssTextField name='email' value={user.email} onChange={(e) => handleChange(e)} id="standard-basic" type="email" label="Email" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}} />
-            <CssTextField name='password' value={user.password} onChange={(e) => handleChange(e)} id="standard-basic" type="password" label="Password" variant="outlined"  InputLabelProps={{style: {color: 'aliceblue'}}}/>
+            <input className='login-input' placeholder='Full Name' name='name' value={user.name} onChange={(e) => handleChange(e)} id="standard-basic" type="name" label="Full Name" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}} />
+            <input className='login-input' placeholder='Phone Number' name='phone' value={user.phone} onChange={(e) => handleChange(e)} id="standard-basic" type="phonenumber" label="Phone Number" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}} />
+            <input className='login-input' placeholder='Email Address' name='email' value={user.email} onChange={(e) => handleChange(e)} id="standard-basic" type="email" label="Email" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}} />
+            <input className='login-input' placeholder='Password' name='password' value={user.password} onChange={(e) => handleChange(e)} id="standard-basic" type="password" label="Password" variant="outlined"  InputLabelProps={{style: {color: 'aliceblue'}}}/>
           </div>
           <div className='signup-buttons'>
               <Button className='seller-button-user' onClick={saveUser} variant='contained'>Sign Up</Button>
