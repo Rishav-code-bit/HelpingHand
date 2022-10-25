229c7173bbb7fd3388660b9b60ef2a789443f805
Ankit Seth <iam4nkit@gmail.com>
2022-10-25 10:30:36 +0530

    fixed login/signup css

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -4,9 +4,20 @@ import image from '../../images/img2.png'
 import { Link } from 'react-router-dom'
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@mui/material';
-
+import { withStyles } from '@material-ui/core/styles';
 
 function Login() {
+
+  const CssTextField = withStyles({
+     root: { 
+      '& label.Mui-focused': { color: 'white', },
+      '& .MuiInput-underline:after': { borderBottomColor: 'yellow', }, 
+      '& .MuiOutlinedInput-root': { '& fieldset': { borderColor: 'white', }, 
+      '&:hover fieldset': { borderColor: 'white', }, 
+      '&.Mui-focused fieldset': { borderColor: 'yellow', }, }, }, 
+  })(TextField);
+
+
   return (
     <div className='login-body-container'>
         <div className='login-form-container'>
@@ -15,12 +26,12 @@ function Login() {
             <h3>Welcome to our Page <Link className='login-link' to = "/signup">SignUp</Link></h3>
           </div>
           <div className='login-form'>
-            <TextField id="standard-basic" type="email" label="Email" variant="outlined" />
-            <TextField className='login-text' id="standard-basic" type="password" label="Password" variant="outlined" />
+            <CssTextField id="standard-basic" type="email" label="Email" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}}/>
+            <CssTextField className='login-text' id="standard-basic" type="password" label="Password" variant="outlined" InputLabelProps={{style: {color: 'aliceblue'}}} />
           </div>
           <div className='login-buttons'>
-              <Button variant='contained'>Sign In</Button>
-              <Button variant='contained'>Sign In as Seller</Button>
+              <Button className='seller-button-user' variant='contained'>Sign In</Button>
+              <Button className='seller-button-seller' variant='contained'>Sign In as Seller</Button>
           </div>
         </div>
         <div className='login-image-container'>
