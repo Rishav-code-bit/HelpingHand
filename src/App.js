6431580df1c28ccbeacf9ab38a28c77e193774f4
Rishav <rishavkumar99057@gmail.com>
2022-10-27 23:27:07 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Homepage/Home';
 import Login from './components/LoginPage/Login';
-import SignUp from './components/LoginPage/SignUp'
-import about from './components/About/abt'
+import SignUp from './components/LoginPage/SignUp';
+import about from './components/About/abt';
+import sidebar from './components/dashboard/dash';
 import LoginService from './services/LoginService';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" exact component={about} />
+        <Route path="/dashboard" exact component={sidebar} />
         <Route path="/signup" exact component={SignUp} />
         <Route path="/login" exact component={Login} />
         <Route path="/loginservice" exact component={LoginService} />
